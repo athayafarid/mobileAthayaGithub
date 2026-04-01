9d5ba493fedb14c28454ab52d78c3747d7739dab
pc21 <ramadani24si@mahasiswa.pcr.ac.id>
2026-04-01 16:15:37 +0700

    Pertemuan 3

---
+++ app/src/main/java/com/example/athaya/pertemuan3/ThirdActivity.kt
@@ -0,0 +1,37 @@
+package com.example.athaya.pertemuan3
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.athaya.R
+import com.example.athaya.databinding.ActivityThirdBinding
+
+class ThirdActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityThirdBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityThirdBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+       // val no: EditText = findViewById(R.id.inputNoTujuan)
+        //val kirim: Button = findViewById(R.id.btnKirim)
+
+        binding.btnKirim.setOnClickListener {
+            val no = binding.inputNoTujuan.text
+            Toast.makeText(this, "pesan berhasi di kirim ke $no", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+}
