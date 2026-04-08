e59de13f9ff180985df851ce7ef75abbd81da44e
pc21 <ramadani24si@mahasiswa.pcr.ac.id>
2026-04-08 16:41:51 +0700

    Penerapan intent

--- app/src/main/java/com/example/athaya/pertemuan3/ThirdActivity.kt
+++ app/src/main/java/com/example/athaya/pertemuan3/ThirdActivity.kt
@@ -19,8 +19,10 @@ class ThirdActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         binding = ActivityThirdBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
